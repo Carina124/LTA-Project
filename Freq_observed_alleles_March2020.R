f8332cdd737b382d721c3f7e3064dce1ccdc9dab
NiquoC <43896041+NiquoC@users.noreply.github.com>
2020-03-12 22:11:45 -0700

    Update Freq_observed_alleles_March2020.R
    Permanent changes:
    1) I turned the code into a function that can use any forensim compatible allele frequency table to calculate the frequency that each allele is actually assigned. 
    
    Debugging sections: 
    There are no debugging sections in this script. It should run with no issues.
    
    Found issues: N/A

--- Freq_observed_alleles_March2020.R
+++ Freq_observed_alleles_March2020.R
@@ -1,9 +1,12 @@
 ######## Checking Assigned Alleles vs. Actual allele Frequency ###########
 
-num_contrib = 100
+#### Niquo 03/12/2020 Turning this code into a function######
 
-file.name <-  "USAf.1_new.csv"
+Freq_observed <- function(file.name, num_contrib){
 
+
+
+######## These need to be intiatied first 
 
 loci.names.inorder <- c("CSF1PO",
                         "D3S1358",
@@ -22,8 +25,11 @@ loci.names.inorder <- c("CSF1PO",
 
 
 ###############################Total contrib function MATT's CODE ##############################################################
+
 library(forensim)
 library(readr)
+
+
 set.seed (123560) 
   #reads in the files we are working with 
   afs_csv <- read.csv(file.name)   
@@ -50,20 +56,12 @@ set.seed (123560)
                            )
   )
 
-####comparing genos.in.mix to imported allele frequncey table 
-# the population name is the only thing not hard coded ###
-genos.in.mix$tab.freq$USAf.1_new.csv[k]
-pop.afs$tab$USAf.1_new.csv[k]
-#### Make a plan####
-# Loop 1: 
 
 
-k =13
-known.contrib.all.atk.pros <- c()
-for(i in 1:(num_contrib)){
-  known.contrib.all.atk.pros = c( known.contrib.all.atk.pros, 
-                                  as.numeric(strsplit(sim.mix$mix.prof[i,k], 
-                                                      "/")[[1]]))}
+  sim.mix <- simumix(genos.in.mix,
+                     ncontri = num_contrib
+  )
+  
 
 #### loop that zeros out all the positions in the tab freq objest's list ##############
 #double square bracket allows you to go into a list. 
@@ -72,13 +70,16 @@ for(i in 1:(num_contrib)){
 #INDEXING genos.in.mix$tab.freq[[allele frequency all loci]][[freq of specific STR]][value at each allele]
 
 # genos.in.mix needs to be saved to a diff name because we still need the original data from the forensim list
-empty_list = genos.in.mix$tab.freq$USAf.1_new.csv
+
+
+
+empty_list = genos.in.mix$tab.freq[[file.name]]
 
 for (locus in 1:13){
   # loop that goes through each STR in the list 
  
   empty_list[[locus]]
-  print(locus)
+  
   
   for (position in 1:length(empty_list[[locus]])){
     
@@ -88,37 +89,54 @@ for (locus in 1:13){
   }
 }
 
-print(empty_list)
-
-###### Next steps: loop that writes ratios of alleles assigned to empty_list
-observed.allele.count[[1]][1] = 0
-observed.allele.count[[1]]["9"]
 
 
-freq_assigned_vec <- c()
-j = 1
 for (k in 1:13){
-for (j in 1:length(genos.in.mix@mix.all[[k]])){
-  #looks in
-  print(paste("allele",sim.mix@mix.all$VWA[j]))
-  current_allele = sim.mix@mix.all$VWA[j]
+  # this looks at each STR in the list 1-13 
+  
+  #generate the allel
+  known.contrib.all.atk.pros <- c()
+  for(i in 1:(num_contrib)){
+    known.contrib.all.atk.pros = c( known.contrib.all.atk.pros, 
+                                    as.numeric(strsplit(sim.mix$mix.prof[i,k], 
+                                                        "/")[[1]]))}
+  #goes into the list at each STR 1=13
+  current_loci = unique(known.contrib.all.atk.pros)
+
+for (j in 1:length(current_loci)){
+  ## this is how many alleles there are in a particular population 
+  #print(paste("allele",
+  current_allele =  current_loci[j]
   count = 0
+
+
   #observed.allele.count[[k]][current_allele] = observed.allele.count[[k]][current_allele] + 1
-  for(i in 1:length(known.contrib.all.atk.pros)){
-    if (known.contrib.all.atk.pros[i] == sim.mix@mix.all$v){
-      count = count + 1
-      observed.allele.count[[k]][current_allele] = observed.allele.count[[k]][current_allele] + 1
-      
-      
+  for(l in 1:length(known.contrib.all.atk.pros)){
+    if (known.contrib.all.atk.pros[l] == current_allele){
+     count = (count + 1) 
+      #empty_list[[k]][current_allele] =  empty_list[[k]][current_allele] + 1
+      #this line goes into a specific allele and frequency @ STR K 
+      #genos.in.mix$tab.freq[[file.name]][[k]][[j]]))
+    # empty_list[[k]][[as.character(current_allele)]] = count
       
     }
     
   }
-  #print(count)
-  freq_assigned = count/(length(known.contrib.all.atk.pros))
-  #print(freq_assigned)
-  freq_assigned_vec <- c(freq_assigned_vec,freq_assigned)
-  #ass.freq.matrix <- ass.freq.matrix[j,1]
+  count = (count+1)/length(known.contrib.all.atk.pros)
+  empty_list[[k]][[as.character(current_allele)]] <- count
 }
   
 }
+  Freq_actually_obs_list <<- empty_list
+}
+
+# Freq_observed (file.name, num_contrib)
+#num_contrib = 100
+#file.name <-  "USAf.1_new.csv"
+
+
+
+
+
+
+
